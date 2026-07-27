1f44b920c520c546f02dece9556ccdc9cd74efd7
activist015 <jibrinfaisal606@gmail.com>
2026-07-27 00:56:33 +0000

    add debug console

--- sabi-app/src/supabaseClient.js
+++ sabi-app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://apjvrfycsvyzzxolxwmd.supabase.co";
-const supabaseKey = "DYsImV4cCI6MjEwMDY3MzMwNn0.1bU2uj4VsXNxCjRPDls4I1Mzoe4sGmYRono7SWB_9PE";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwanZyZnljc3Z5enp4b2x4d21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwOTczMDYsImV4cCI6MjEwMDY3MzMwNn0.1bU2uj4VsXNxCjRPDls4I1Mzoe4sGmYRono7SWB_9PE";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
